831e89077080b079d4a05613811cd098141dfa8a
Chris I Powell <chris.i.powell@gmail.com>
2014-03-27 16:21:59 +0000

    PhyloCanvas - autoload buttons handlers based on id

--- app/js/phylocanvas_demo.js
+++ app/js/phylocanvas_demo.js
@@ -1,11 +1,20 @@
 (function(){
     "use strict";
-
-    var PC = PhyloCanvas;
+    var PC = PhyloCanvas,
+        $ = document.querySelectorAll.bind(document);
 
     function init(){
         var phylo = new PC.Tree('phylo');
         phylo.load('data/EARSS.nwk');
+
+        var buttons = $('button.tree-control');
+
+        for( var b = buttons.length; b--; )
+        {
+            console.debug(buttons.length);
+
+            buttons[b].addEventListener('click', phylo[buttons[b].id].bind(phylo));
+        }
     }
 
     document.body.onload = init();
