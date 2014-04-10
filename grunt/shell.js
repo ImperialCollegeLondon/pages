9f9d5b830b3402bb5068081480f2ad5f8a903114
Chris I Powell <chris.i.powell@gmail.com>
2014-04-10 13:21:56 +0100

    new build

--- grunt/shell.js
+++ grunt/shell.js
@@ -1,9 +1,21 @@
 module.exports = {
+    predeploy: {
+        options : {
+            stdout: true,
+            execOptions: {
+                  cwd: 'dist'
+            }
+        },
+        command : 'git commit -a -m "new build"'
+    },
     deploy: {
         options : {
-            stdout: true
+            stdout: true,
+            execOptions: {
+                  cwd: 'dist'
+            }
         },
-        command : 'git subtree push --prefix dist gh-pages master'
+        command : 'git push'
     },
     save_build: {
         options : {
