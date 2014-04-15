4d0dcb36e00cc0daf6336ce084ffc84a709c7211
Chris I Powell <chris.i.powell@gmail.com>
2014-04-15 11:54:58 +0100

    new build

--- grunt/copy.js
+++ grunt/copy.js
@@ -2,7 +2,7 @@ module.exports = {
     dev: {
         files : [
             {expand: true, cwd : 'app/pages', src: ['**/*.html', '**/*.nwk'], dest: 'build/'},
-            {expand: true, cwd : 'app', src: ['**/*.js', '**/*.css'], dest: 'build/'}
+            {expand: true, cwd : 'app', src: ['**/*.js', '**/*.css','images/*.svg'], dest: 'build/'}
         ]
     },
     build: {
